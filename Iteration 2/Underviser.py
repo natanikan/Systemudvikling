609760d71e51088844b94ea0f803e2017079a0eb
AntonKAndersen <anton.k.andersen@gmail.com>
2022-04-07 09:25:45 +0200

    Oprydning

--- Iteration 2/Underviser.py
+++
@@ -1,5 +0,0 @@
-class Underviser:
-    def __init__(self, start_tidspunkt: str):
-        self.__start_tidspunkt = start_tidspunkt
-
-    def get_start_tidspunkt(self): return self.__start_tidspunkt
